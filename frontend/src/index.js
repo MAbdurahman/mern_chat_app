0371787c74cf054cc896aca7b06bfbdedc78eacd
MAbdurahman <mdbdrrhm@gmail.com>
2022-03-11 11:43:58 -0600

    Update frontend/index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import './styles/global-styles.css';
-
 import App from './app/App';
 
 
