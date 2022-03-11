9146cd246d66ec68421a10cfa291aa44f97879f7
MAbdurahman <mdbdrrhm@gmail.com>
2022-03-11 11:34:33 -0600

    Add packages to frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import './styles/global-styles.css';
 
 import App from './app/App';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<ChakraProvider>
+			<App />
+		</ChakraProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
