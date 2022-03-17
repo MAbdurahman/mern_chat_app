ff89c086984da1db97c767dc3cd7d0361b978108
MAbdurahman <mdbdrrhm@gmail.com>
2022-03-17 16:12:29 -0500

    Complete most of SideDrawer.jsx and MyChats.jsx

--- frontend/src/components/MyChats.jsx
+++ frontend/src/components/MyChats.jsx
@@ -3,8 +3,9 @@ import { AddIcon } from '@chakra-ui/icons';
 import { Box, Stack, Text } from '@chakra-ui/layout';
 import { useToast } from '@chakra-ui/toast';
 import axios from 'axios';
-import { Button } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react/';
 import { ChatState } from './../context/chatContext';
+import { getSender } from '../config/chatLogicsConfig';
 import ChatLoading from './ChatLoading';
 
 export default function MyChats({ fetchAgain }) {
@@ -15,7 +16,6 @@ export default function MyChats({ fetchAgain }) {
 
 	//**************** functions ****************//
 	const fetchChats = async () => {
-		
 		try {
 			const config = {
 				headers: {
@@ -25,7 +25,6 @@ export default function MyChats({ fetchAgain }) {
 
 			const { data } = await axios.get('/api/v1/chats', config);
 			setChats(data);
-
 		} catch (error) {
 			toast({
 				title: 'Error Occurred!',
@@ -44,5 +43,81 @@ export default function MyChats({ fetchAgain }) {
 		// eslint-disable-next-line
 	}, [fetchAgain]);
 
-	return <div>MyChats</div>;
+	return (
+		<Box
+			d={{ base: selectedChat ? 'none' : 'flex', md: 'flex' }}
+			flexDir='column'
+			alignItems='center'
+			p={3}
+			bg='white'
+			w={{ base: '100%', md: '31%' }}
+			borderRadius='lg'
+			borderWidth='1px'
+		>
+			<Box
+				pb={3}
+				px={3}
+				fontSize={{ base: '28px', md: '30px' }}
+				fontFamily='Montserrat'
+				d='flex'
+				w='100%'
+				justifyContent='space-between'
+				alignItems='center'
+			>
+				My Chats
+				<Button
+					d='flex'
+					fontFamily='Montserrat'
+					fontSize={{ base: '17px', md: '10px', lg: '17px' }}
+					rightIcon={<AddIcon />}
+				>
+					New Group Chat
+				</Button>
+			</Box>
+			<Box
+				d='flex'
+				flexDir='column'
+				p={3}
+				bg='#F8F8F8'
+				w='100%'
+				h='100%'
+				borderRadius='lg'
+				overflowY='hidden'
+			>
+				{chats ? (
+					<Stack overflowY='scroll'>
+						{chats.map(chat => (
+							<Box
+								onClick={() => setSelectedChat(chat)}
+								cursor='pointer'
+								bg={selectedChat === chat ? '#007dc6' : '#E8E8E8'}
+								color={selectedChat === chat ? 'white' : 'black'}
+								px={3}
+								py={2}
+								borderRadius='lg'
+								key={chat._id}
+							>
+								<Text fontFamily='Anonymous Pro'>
+									{!chat.isGroupChat
+										? getSender(loggedUser, chat.users)
+										: chat.chatName}
+								</Text>
+								{chat.latestMessage && (
+									<Text fontSize='xs' fontFamily='Anonymous Pro'>
+										<b>{chat.latestMessage.sender.name} : </b>
+										{chat.latestMessage.content.length > 50
+											? chat.latestMessage.content.substring(0, 51) +
+											'...'
+											: chat.latestMessage.content}
+									</Text>
+								)}
+							</Box>
+						))}
+					</Stack>
+				) : (
+					<ChatLoading />
+				)}
+			</Box>
+		</Box>
+	);
 }
